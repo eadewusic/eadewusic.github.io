c37a8acd623bad9aa22f09cd0916eaa63a13a8f0
Eunice Adewusi <e.adewusi@alustudent.com>
2024-02-14 17:43:44 +0100

    edited files to an extent

---
+++ Portfolio/script.js
@@ -0,0 +1,8 @@
+<script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.12"></script>
+
+var typed = new Typed(".auto-type", {
+    strings: ["Content Creator", "Front-End Developer", "Content Creator", "Social Media Manager", "Tech Enthusiast", "Global ChangeMaker"],
+    typeSpeed: 150,
+    backSpeed: 150,
+    loop: true
+})
